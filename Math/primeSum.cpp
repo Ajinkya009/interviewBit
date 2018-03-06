f7abd5ad8dfa206c9bfd84b31094152c410fe2ee
Ajinkya Deshmukh <ajinkya@Ajinkyas-MacBook-Air.local>
2018-03-06 17:28:52 +0530

    math->prime sum solution

--- Math/primeSum.cpp
+++ Math/primeSum.cpp
@@ -1,3 +1,17 @@
+/*Problem: Given an even number ( greater than 2 ), return two prime numbers whose sum will be equal to given number.
+
+Input : 4
+Output: 2 + 2 = 4
+
+If there are more than one solutions possible, return the lexicographically smaller solution.
+
+If [a, b] is one solution with a <= b,
+and [c,d] is another solution with c <= d, then
+
+[a, b] < [c, d] 
+
+If a < c OR a==c AND b < d. */
+
 bool isPrime(int n){
     
     if (n <= 1)  return false;
